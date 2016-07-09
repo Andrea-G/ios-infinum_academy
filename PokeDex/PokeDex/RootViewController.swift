0da82f15cd05a2ca4403366ccf8310d825b095a7
andrea <andrea.gradecak@gmail.com>
2016-07-09 12:12:44 +0200

    dz1

---
+++ PokeDex/PokeDex/RootViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RootViewController.swift
+//  PokeDex
+//
+//  Created by Infinum on 09/07/16.
+//  Copyright © 2016 Infinum. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logInButtonClick(sender: UIButton) {
+        
+        print(usernameTextField.text)
+        print(passwordTextField.text)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
